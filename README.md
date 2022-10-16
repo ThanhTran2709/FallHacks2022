6ccf7663b6d163c5639ba80ef066bf78ea0e7cbe
Jason Nguyen <khaivnguyen02@gmail.com>
2022-10-15 18:42:00 -0700

    As complete as time allows

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# FallHacks2022
+# CrunchTime
+
+This website let you manage tasks and deadlines. The original intent was to make an app that helps with procrastination, hence the motto "As you get passive, we get aggressive"
+
+Tools: html, css, javascript
+
+Instruction: Open landingPage.html file on any browsers and log in with any user information.It will redirect you to index.html. Input your information (Attention: the format for deadlines is: javascript date time format. Ex: Oct 22, 2022 00:00:00 . The format for allowed apps: Paste the URL of the website you want to launch). Press submit to save the information. Press launch button next to the task name displayed to open the app (Only works with one URL).
+
+## Name 
+- Khai (Jason) Nguyen
+- Huu Hoang Duong Nguyen
+- Duc Manh To
+- Huu Viet Hoang Nguyen
+- Minh Thanh Tran
--- scripts.js
+++ scripts.js
@@ -1,44 +1,92 @@
 const taskList = [];
-var list = document.createElement('list');
-
-class Task{
-    constructor(taskName, description, deadline, timeBlock, allowedApps){
-        this.taskName = taskName;
-        this.description = description;
-        this.deadline = deadline;
-        this,timeBlock = timeBlock;
-        this.allowedApps = allowedApps;
-    }
+var list = document.createElement("ul");
+var timerList = document.createElement('ul');
+class Task {
+  constructor(taskName, description, deadline, timeBlock, allowedApps) {
+    this.taskName = taskName;
+    this.description = description;
+    this.deadline = deadline;
+    this.timeBlock = timeBlock;
+    this.allowedApps = allowedApps;
+  }
 }
 
 function addTask() {
-    var taskName = document.getElementById('tName').value;
-    var description = document.getElementById('tDesc').value;
-    var deadline = document.getElementById('tDL').value;
-    var timeBlock = document.getElementById('tTimeBlock').value;
-    var allowedApps = document.getElementById('tApps').value;
+  var taskName = document.getElementById("tName").value;
+  var description = document.getElementById("tDesc").value;
+  var deadline = document.getElementById("tDL").value;
+  var timeBlock = document.getElementById("tTimeBlock").value;
+  var allowedApps = document.getElementById("tApps").value;
 
-    let task = new Task(taskName, description, deadline, timeBlock, allowedApps);
-    taskList.push(task)
+  let task = new Task(taskName, description, deadline, timeBlock, allowedApps);
+  taskList.push(task);
 
-    var item = document.createElement('li');
+  var item = document.createElement("li");
 
-    // Set its contents:
-    item.appendChild(document.createTextNode(task.taskName));
+  // Set its contents:
+  item.appendChild(document.createTextNode(task.taskName));
 
-    // Add it to the list:
-    list.appendChild(item);
+  // Add it to the list:
+  list.appendChild(item);
 
-    document.getElementById('foo').appendChild(list);
-    //for (var i = 0; i < taskList.length; i++)
-    //    console.log(taskList[i].allowedApps);
-}
-
-function launch(){
-    urls = taskList[0].allowedApps.split(",");
-    for (var i = 0; i < urls.length; i++){
+  document.getElementById("tasks-list").appendChild(list);
+  //   //for (var i = 0; i < taskList.length; i++)
+  //   //    console.log(taskList[i].allowedApps);
+  item.setAttribute("class", "task-name");
+  //Add button
+  var btn = document.createElement("button");
+  btn.innerHTML = "Launch";
+  document.getElementById("tasks-list").appendChild(btn);
+    btn.addEventListener("click", () => {
+      urls = task.allowedApps.split(",");
+      for (var i = 0; i < urls.length; i++) {
         console.log(urls[i]);
-        window.open(urls[i]);
+        window.open(urls[i], '_blank');
+      }
+    });
+//   btn.addEventListener("click", launch);
+  btn.setAttribute("class", "launch-btn");
+
+  var timerItem = document.createElement('li');
+    timerItem.appendChild(document.createTextNode(""));
+    timerList.appendChild(timerItem);
+    document.getElementById("foo").appendChild(timerList); 
+
+    var countDownDate = new Date(deadline).getTime();
+    
+    var countDownEvent = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    timerItem.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(countDownEvent);
+        document.getElementById("demo").innerHTML = "EXPIRED";
     }
+    }, 1000);
 }
+
+// function launch() {
+//   urls = taskList[0].allowedApps.split(",");
+//   for (var i = 0; i < urls.length; i++) {
+//     console.log(urls[i]);
+//     window.open(urls[i]);
+//   }
+// }
+
+
 
